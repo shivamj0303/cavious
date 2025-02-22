0966d919844b59b57b7d58a392e4e077a0ccd475
shivamj0303 <jangidshivam2002@gmail.com>
2025-02-22 21:42:24 +0530

    Add FastAPI boilerplate with a simple test endpoint

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-
+from intents import recognize_intent
 app = FastAPI()
 
 @app.get("/ping")
 def ping():
     return {"message": "Server is running"}
+
+@app.post("/process")
+def process_text(user_input: str):
+    intent = recognize_intent(user_input)
+    return {"user_input": user_input, "intent": intent}
